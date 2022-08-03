0bbecdbf19f3cf672065f8b086ed46ea5e858157
sweetnectar <93552075+sweetnectar@users.noreply.github.com>
2022-08-03 12:00:09 -0700

    Center input fields and add login/register buttons

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,14 +1,52 @@
-import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import {
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 const LoginScreen = () => {
   return (
-    <View>
-      <Text>Login Screen</Text>
-    </View>
+    <KeyboardAvoidingView style={styles.container} behavior="padding">
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Email"
+          // value={}
+          // onChangeText={text =>}
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Password"
+          // value={}
+          // onChangeText={text =>}
+          style={styles.input}
+          secureTextEntry
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity onPress={() => {}} style={styles.button}>
+          <Text style={styles.button}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {}}
+          style={[styles.button, styles.buttonOutline]}
+        >
+          <Text style={styles.buttonOutlineText}>Register</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
   );
 };
 
 export default LoginScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
