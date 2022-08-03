328c8f667a6c7818ac84b1a9c763adff96bf2488
sweetnectar <93552075+sweetnectar@users.noreply.github.com>
2022-08-03 12:17:00 -0700

    Add style to login and register buttons

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -28,7 +28,7 @@ const LoginScreen = () => {
       </View>
       <View style={styles.buttonContainer}>
         <TouchableOpacity onPress={() => {}} style={styles.button}>
-          <Text style={styles.button}>Login</Text>
+          <Text style={styles.buttonText}>Login</Text>
         </TouchableOpacity>
         <TouchableOpacity
           onPress={() => {}}
@@ -48,5 +48,42 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  inputContainer: {
+    width: "80%",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5,
+  },
+  buttonContainer: {
+    width: "60%",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 40,
+  },
+  button: {
+    backgroundColor: "#B0DAC2",
+    width: "100%",
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  buttonOutline: {
+    backgroundColor: "white",
+    marginTop: 5,
+    borderColor: "#B0DAC2",
+    borderWidth: 2,
+  },
+  buttonText: {
+    fontWeight: "700",
+    fontSize: 16,
+  },
+  buttonOutlineText: {
+    fontWeight: "700",
+    fontSize: 16,
   },
 });
